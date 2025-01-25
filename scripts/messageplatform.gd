40379d37af72453a193b0abb4349c0b2b6de8caf
Alex Chow <alex0091845@gmail.com>
2025-01-25 11:17:04 -0800

    dynamic collision box

--- scripts/messageplatform.gd
+++ scripts/messageplatform.gd
@@ -19,6 +19,7 @@ var msgLabel
 @export var msgText : String = "Hello! This is a message!"
 @export var msgType : Type
 
+
 func _ready():
 	#Declare variables
 	msg = $msgBox
@@ -30,5 +31,20 @@ func _ready():
 	#Text color
 	msgLabel.add_theme_color_override("font_color", color_map[msgType][1])
 
+
 func _process(delta):
 	msgLabel.text = msgText
+
+	if Engine.is_editor_hint():
+		print("running??")
+		_size_collision_shape()
+	
+
+func _size_collision_shape():
+	var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
+	
+	collision_shape_2d.shape.size.x = size.y
+	collision_shape_2d.shape.size.y = size.x
+	
+	collision_shape_2d.position.x = size.x / 2
+	collision_shape_2d.position.y = size.y / 2
