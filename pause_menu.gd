1d87b7aaeaa65f3442d03938137fa26c40c2d3ba
Lou <louistabalon@gmail.com>
2025-01-26 13:03:34 -0800

    fixed quit button

--- pause_menu.gd
+++ pause_menu.gd
@@ -25,7 +25,12 @@ func _on_options_pressed() -> void:
 	print("OPTIONS MENU!")
 
 func _on_quit_pressed() -> void:
+<<<<<<< Updated upstream
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+=======
+	get_tree().quit()
+	print("MAIN MENU!")
+>>>>>>> Stashed changes
 
 func _process(delta):
 	testEsc()
