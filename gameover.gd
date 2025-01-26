2830dd9e2459416b118dac7d95fc579b5d3239f3
Lou <louistabalon@gmail.com>
2025-01-26 13:20:38 -0800

    game over menu

---
+++ gameover.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit()
