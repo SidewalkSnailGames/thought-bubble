ab83e858db69bc4eb02da4e6cbbc569e091cb2a0
Michael Scott <mikescott3595@gmail.com>
2025-01-26 15:00:49 -0800

    Fixed level respawn points and collision

--- scenes/spikes.gd
+++ scenes/spikes.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @onready var player = $Player
-@onready var respawn_point: Node2D = $"../../Respawn Point"
+@export var respawn_point: Node2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,5 +15,5 @@ func _on_body_entered(body: Node2D) -> void:
 		#print(str(body.position.y) + " " + str(respawn_point.position.y))
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
