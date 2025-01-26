0e79de14980fd1f49036b463bd060869f48db144
Elliott <darthmagicgaming@gmail.com>
2025-01-25 16:49:54 -0800

    camera limiter

---
+++ scripts/cameralimiter.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var camera = $"../Node2D/Camera2D"
+
+func _ready():
+	var area_size = $CollisionShape2D.shape.extents * 2
+	
+	camera.limit_left = $CollisionShape2D.global_position.x - area_size.x / 2
+	camera.limit_top = $CollisionShape2D.global_position.y - area_size.y / 2
+	camera.limit_right = $CollisionShape2D.global_position.x + area_size.x / 2
+	camera.limit_bottom = $CollisionShape2D.global_position.y + area_size.y / 2
