a2d4420016da1430060e048fb1d71f45777ed61f
matthew jones <mattjonesy724@gmail.com>
2025-01-25 17:21:16 -0800

    Made 1st level and Bubble pieces

---
+++ scripts/bubble_choice.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+@onready var level_choices: Node2D = $".."
+@export var text: String = ""
+@onready var label: Label = $Label
+@export var moral: morality
+static var has_chosen
+static var choice
+
+func _ready():
+	label.text = text
+
+enum morality{Good, 
+			  Bad
+			  }
+
+#func disable(child):
+	#child.monitoring = false
+	#child.visible = false
+
+func disable_children():
+	for child in level_choices.get_children():
+		print(child.name)
+		child.set_deferred("monitoring", false)
+		print(child.monitoring)
+		child.set_deferred("visible", false)
+		#child.visible = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if moral == morality.Good:
+		choice = "Good"
+		print(choice)
+		
+	else:
+		choice = "Bad"
+		print(choice)
+	
+	disable_children()
