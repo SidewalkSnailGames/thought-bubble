3e720f22477d614073d54d9e9cf1b518cba3415b
Alex Chow <alex0091845@gmail.com>
2025-01-26 10:46:50 -0800

    double jump fixed

--- scripts/player.gd
+++ scripts/player.gd
@@ -21,7 +21,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and jump_count < max_jump and can_jump:
 		animator.play("jump")
 		velocity.y = JUMP_VELOCITY
-		jump_count =+ 1
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
